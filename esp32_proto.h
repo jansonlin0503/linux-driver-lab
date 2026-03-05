8246bdd3fb7c8cb3b12cc21eb1b165c7a468d8ea
Tao Lin <jansonlin0503@gmail.com>
2026-03-05 15:43:55 +0800

    Add esp32 control protocol

---
+++ esp32_proto.h
@@ -0,0 +1,32 @@
+#ifndef ESP32_PROTO_H
+#define ESP32_PROTO_H
+
+#define ESP32_GPIO_IN 0
+#define ESP32_GPIO_OUT 4
+
+#define HOST_GPIO_IN 538
+#define HOST_GPIO_OUT 529
+
+typedef enum power_state{
+	P_STATE_ACTIVE = 0,
+	P_STATE_LIGHT_SLEEP,
+	P_STATE_DEEP_SLEEP,
+};
+
+/* I2C protocol: cmd(msg) + 1 byte data
+ * DEV_GET_POWER_MODE: no further data required 
+ * DEV_SET_LIGHT_POWER_MODE: with p_stat_t data
+ *
+ * DEV_POWER_MODE: power mode message */
+
+enum device_cmd {
+	DEV_GET_POWER_MODE = 0x01,
+	DEV_SET_POWER_MODE = 0x02,
+};
+
+enum device_msg {
+	DEV_POWER_MODE_MSG = 0x11,
+}
+
+#endif
+
